8bb5aa2411dfbfb7ff289e03a9d44b264310143c
RedRossum <redrossum@gmail.com>
2024-02-12 04:22:47 +0300

    feat: add solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -87,8 +87,16 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  return (
+    a + b > c &&
+    b + c > a &&
+    a + c > b &&
+    a > 0 &&
+    b > 0 &&
+    c > 0 &&
+    (a === b || b === c || a === c)
+  );
 }
 
 /**
